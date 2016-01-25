d95a82b5fadeafc4200cabf46c3d0d0fcd4c1121
Manu Uzkudun Egiguren <satkiran@MacBook-Pro-de-Manu.local>
2016-01-25 12:47:30 +0100

    Child process added to the file watcher program

--- file-system/watcher.js
+++ file-system/watcher.js
@@ -1,8 +1,23 @@
+"use strict"; 
 
-const fs = require('fs');
+const fs = require('fs'),
+      spawn = require('child_process').spawn,
+      filename = process.argv[2];
 
-fs.watch('target.txt', function() {
-	console.log("File 'target.txt' just changed!");
+if (!filename) {
+  throw Error("A file to watch must be specified!");
+}
+
+fs.watch(filename, function() {
+  // first param of spawn is the program name
+  // second param is an array of command-line arguments.
+  // object returned by spawn() is a ChildProcess
+  let ls = spawn('ls', ['-lh', filename]);
+  // stdin, stdout, and stderr properties are Streams 
+  // that can be used to read or write data.
+  // we send standard output from the child process directly
+  // to our own standard output stream.
+  ls.stdout.pipe(process.stdout);
 });
 
-console.log("Now watching target.txt for changes...");
+console.log("Now watching " + filename + " for changes...");
